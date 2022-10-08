da80ad4beb42353157ecfdb513e0189d44b1bc4a
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2022-10-09 01:09:34 +0200

    Готовая 6 лаба

--- 6_lab/6_lab.cpp
+++ 6_lab/6_lab.cpp
@@ -1,44 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
 
-//void readFile() {
-////	здесь чтение файла в отдельном файле
-//}
+#include <read_file.hpp>
+#include <process_file.hpp>
+#include <output_file.hpp>
 
-void sort(int matrix[100][100], int n, int m) {
-	for (int a = 0; a < n; a++) {
-		for (int j = 0; j < m - 1; j++) {
-			for (int k = 0; k < m - 1; k++) {
-				if (matrix[a][k] > matrix[a][k + 1]) {
-					int tempNumber = matrix[a][k + 1];
-					matrix[a][k + 1] = matrix[a][k];
-					matrix[a][k] = matrix[a][tempNumber];
-				}
-			}
-		}
-	}
-	
-}
-
-void processingFile() {
-	//Второй основной файл включает в себя обработку полученных данных
-	int n = 100; //строки
-	int m = 100; //столбцы
-	int matrix[100][100] = { {0} };
-	int maxElement;
-	int minElement;
-	//sort(matrix[100][100], n, m);
-	if (minElement + maxElement == 0) {
-		for (int i = 0; i < n; i++) {
-
-		}
-	}
-
-}
-
-//void whiteFile() {
-//	//вывод полученного результата в консоль
-//}
 
 int main() {
+	setlocale(LC_ALL, "RUS");
+	int n = 3;
+	int m = 3;
+	int matrix[100][100];
 
+
+	read::readFile(n, m, matrix);
+	
+	std::cout << "Исходная матрица:" << std::endl;
+	std::cout << std::endl;
+	output::outputFile(n, m, matrix);
+	std::cout << std::endl;
+	std::cout << std::endl;
+	
+	process::processFile(n, m, matrix);
+
+	std::cout << "Полученная матрица:" << std::endl;
+	std::cout << std::endl;
+	output::outputFile(n, m, matrix);
 }
