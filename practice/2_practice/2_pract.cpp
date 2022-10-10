d08b459afca12aec73da7af501c8f8d8cd165e31
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2022-10-10 20:34:57 +0200

    переделано под строки, надо переделать вывод

--- practice/2_practice/2_pract.cpp
+++ practice/2_practice/2_pract.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-	long long A;
-	long long B;
-	long long res = 0;
+	string A;
+	string B;
+	char res[1000] = {};
 	std::cin >> A;
 	std::cin >> B;
-	int minSize = 0;
-	long long tempA = A;
-	long long tempB = A;
-	while (tempA > 0 && tempB > 0) {
-		tempA /= 10;
-		tempB /= 10;
-		minSize++;
-	}
+	int minSize = min(A.length(), B.length());
+	int maxSize = max(A.length(), B.length());
+	int tempLen = minSize;
+	int temp = 0;
+	/*tempA = A[3 - 1];
+	cout << (tempA.c_str() - '0');*/
 	for (int i = 0; i < minSize; i++) {
 		//std::cout << A << " " << A % 10 << " " << 123 % 10;
-		tempA = A % 10;
-		tempB = B % 10;
-		A /= 10;
-		B /= 10;
-		res += (tempA + tempB) * pow(10, i);
+		char tempA = A[tempLen - 1];
+		char tempB = B[tempLen - 1];
+		tempLen -= 1;
+		A = A.substr(0, tempLen);
+		B = B.substr(0, tempLen);
+		if ((tempA - '0') + (tempB - '0') <= 9) {
+			res[1000 - i - 1] = (char) (tempA - '0') + (tempB - '0') + temp;
+			temp = 0;
+		}
+		else if ((tempA - '0') + (tempB - '0') > 9) {
+			temp = ((tempA - '0') + (tempB - '0')) / 10;
+			res[1000 - i - 1] = (char) (tempA - '0') + (tempB - '0');
+		}
 	}
-	res += (A + B) * pow(10, minSize);
-	std::cout << res;
+	bool flag = false;
+	//TODO исправить вывод
+	for (int i = 1; i < 1000; i++) {
+		if (flag) {
+			cout << res[i]- '0';
+		}
+		else if (res[i] != '\0' && !flag) {
+			flag = true;
+			cout << (res[i] - '0');
+		}
+
+	}
 }
