e909985c6c1a375d9388a5ebbe151083681d4266
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2022-10-14 13:05:57 +0200

    Не работает

--- practice/2_practice/2_pract.cpp
+++ practice/2_practice/2_pract.cpp
@@ -2,31 +2,28 @@
 #include <string>
 using namespace std;
 
-int add(string A, string B, char res[1000]) {
-	string newA = A;
-	string newB = B;
-	res[1000] = {};
-	int minSize = min(A.length(), B.length());
-	int maxSize = max(A.length(), B.length());
+int add(char A[1000], char B[1000], char res[1001], int lenA, int lenB) {
+	//todo сделать аргумент длин массивов
+	res[1001] = {};
+	int minSize = min(lenA, lenB);
+	int maxSize = max(lenA, lenB);
 	int tempLen = minSize;
 	int temp = 0;
 	for (int i = 0; i < minSize; i++) {
 		char tempA;
 		char tempB;
-		if (A.length() > B.length()) {
-			tempA = A[tempLen - 1 + maxSize - minSize];
-			tempB = B[tempLen - 1];
+
+		if (lenA > lenB) {
+			tempA = A[tempLen - 1 + maxSize - minSize - i];
+			tempB = B[tempLen - 1 - i];
 			tempLen--;
-			A = A.substr(0, tempLen + maxSize - minSize);
-			B = B.substr(0, tempLen);
 		}
 		else {
-			tempA = A[tempLen - 1];
-			tempB = B[tempLen - 1 + maxSize - minSize];
+			tempA = A[tempLen - 1 - i];
+			tempB = B[tempLen - 1 + maxSize - minSize - i];
 			tempLen--;
-			B = B.substr(0, tempLen + maxSize - minSize);
-			A = A.substr(0, tempLen);
 		}
+
 		if ((tempA - '0') + (tempB - '0') + temp <= 9) {
 			res[i] = (tempA - '0') + (tempB - '0') + temp;
 			temp = 0;
@@ -36,16 +33,15 @@ int add(string A, string B, char res[1000]) {
 			temp = ((tempA - '0') + (tempB - '0') + temp) / 10;
 		}
 	}
-	if (newA.length() == newB.length()) {
+	if (lenA == lenB) {
 		if (temp != 0) {
 			res[minSize] = temp;
 		}
 	}
-	if (newA.length() > newB.length()) {
+	if (lenB > lenB) {
 		for (int i = 0; i < maxSize - minSize; i++) {
-			char tempA = newA[maxSize - minSize - 1];
+			char tempA = A[maxSize - minSize - 1 - i];
 			tempLen--;
-			newA = newA.substr(0, tempLen);
 			if (((tempA - '0') + temp) <= 9) {
 				res[i + minSize] = ((tempA - '0') + temp);
 			}
@@ -58,10 +54,8 @@ int add(string A, string B, char res[1000]) {
 	}
 	else {
 		for (int i = 0; i < maxSize - minSize; i++) {
-			char tempB = newB[maxSize - minSize - 1];
+			char tempB = B[maxSize - minSize - 1 - i];
 			tempLen--;
-			newB = newB.substr(0, tempLen);
-
 			if (((tempB - '0') + temp) <= 9) {
 				res[i + minSize] = ((tempB - '0') + temp);
 			}
@@ -75,50 +69,59 @@ int add(string A, string B, char res[1000]) {
 	
 }
 
-void multiply(string A, string B, char res[1000]) {
-	string multiplySumma = "";
-	for (int i = 0; i < 1000; i++) {
-		multiplySumma += '0';
-	}
-	for (int i = A.length() - 1; i > - 1; i--) {
-		int tempSumma = 0;
-		for (int j = B.length(), int count = 0; j > 0; j--, count++) {
-			tempSumma += (A[i] - '0') * (B[j] - '0') * pow(10, count);
-		}
-		int temp = add(multiplySumma, to_string(tempSumma), res);
-		if (temp != 0) {
-			string temp;
-			for (int j = to_string(tempSumma).length(); j > -1 ; i--) {
-				
-			}
-		}
-		else {
-			for (int j = to_string(tempSumma).length() - 1; j > -1; i--) {
-
-			}
-		}
-	}
-}
+//void multiply(string A, string B, char res[1000]) {
+//	string multiplySumma = "";
+//	for (int i = 0; i < 1000; i++) {
+//		multiplySumma += '0';
+//	}
+//	for (int i = A.length() - 1; i > - 1; i--) {
+//		int tempSumma = 0;
+//		for (int j = B.length(), int count = 0; j > 0; j--, count++) {
+//			tempSumma += (A[i] - '0') * (B[j] - '0') * pow(10, count);
+//		}
+//		int temp = add(multiplySumma, to_string(tempSumma), res);
+//		if (temp != 0) {
+//			string temp;
+//			for (int j = to_string(tempSumma).length(); j > -1 ; i--) {
+//				
+//			}
+//		}
+//		else {
+//			for (int j = to_string(tempSumma).length() - 1; j > -1; i--) {
+//
+//			}
+//		}
+//	}
+//}
 
 int main() {
-	string A;
-	string B;
+	string firstA;
+	string firstB;
+	char A[1000] = {};
+	char B[1000] = {};
 	char res[1000] = {};
-	std::cin >> A;
-	std::cin >> B;
-	int temp = add(A, B, res);
+	std::cin >> firstA;
+	std::cin >> firstB;
+	for (int i = 0; i < firstA.length(); i++) {
+		A[i] = firstA[i];
+	}
+	for (int i = 0; i < firstB.length(); i++) {
+		B[i] = firstB[i];
+	}
+	int temp = add(A, B, res, firstA.length(), firstB.length());
+	
 	if (temp != 0) {
-		for (int i = max(A.length(), B.length()); i != -1; i--) {
+		for (int i = max(strlen(A), strlen(B)); i != -1; i--) {
 			cout << (int)res[i];
 		}
 	}
-	else {
-		for (int i = max(A.length(), B.length()) - 1; i != -1; i--) {
 
+	else {
+		for (int i = max(strlen(A), strlen(B)); i != -1; i--) {
 			cout << (int)res[i];
 		}
 	}
 
 	cout << endl;
-	multiply(A, B, res);
+	//multiply(A, B, res);
 }
