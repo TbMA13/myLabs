386fe34e63bb1ca7de088e57597640f9d931f7bf
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2022-10-20 08:50:13 +0200

    Мелкие правки

--- practice/2_practice/2_pract.cpp
+++ practice/2_practice/2_pract.cpp
@@ -5,16 +5,18 @@
 void add(std::string A, std::string B, std::string &res) {
 	int i = 0;
 	res = "";
+	int temp = 0;
+	int plus = 0;
 	int maxLength = std::max(A.length(), B.length());
 	for (int j = 0; j < maxLength; j++) {
 		res += "";
 	}
+
 	std::string invertA = A;
 	std::string invertB = B;
 	std::reverse(invertA.begin(), invertA.end());
 	std::reverse(invertB.begin(), invertB.end());
-	int temp = 0;
-	int plus = 0;
+
 	while (i < invertA.length() && i < invertB.length()) {
 		temp = (invertA[i] - '0') + (invertB[i] - '0') + plus;
 		res += (temp % 10) + '0';
@@ -26,6 +28,7 @@ void add(std::string A, std::string B, std::string &res) {
 		}
 		i++;
 	}
+
 	while (i < invertA.length()) {
 		temp = (invertA[i] - '0') + plus;
 		res += (temp % 10) + '0';
@@ -37,6 +40,7 @@ void add(std::string A, std::string B, std::string &res) {
 		}
 		i++;
 	}
+
 	while (i < invertB.length()) {
 		temp = (invertB[i] - '0') + plus;
 		res += (temp % 10) + '0';
@@ -48,47 +52,57 @@ void add(std::string A, std::string B, std::string &res) {
 		}
 		i++;
 	}
+
 	if (plus) {
 		res += plus + '0';
 	}
+
 	std::reverse(res.begin(), res.end());
 }
 
 void multiply(std::string multiA, std::string multiB, std::string& multiResult) {
-	multiResult = "0";
-	//std::string invertA = A;
-	//std::string invertB = B;
-	//std::reverse(invertA.begin(), invertA.end());
-	//std::reverse(invertB.begin(), invertB.end());
 	if (multiA == "0" || multiB == "0") {
 		multiResult = "0";
 		return;
 	}
+
+	multiResult = "0";
 	std::string invertMultiA = multiA;
 	std::reverse(invertMultiA.begin(), invertMultiA.end());
+
 	for (int i = 0; i < multiA.length(); i++) {
-		//std::string firstResult = "0";
-		std::string secondResult = "0";
+		std::string firstResult = "0";
+
 		for (int j = 0; j < invertMultiA[i] - '0'; j++) {
-			add(secondResult, multiB, secondResult);
+			add(firstResult, multiB, firstResult);
 		}
+
 		for (int j = 0; j < i; j++) {
-			secondResult = secondResult + "0";
+			firstResult = firstResult + "0";
 		}
-		add(multiResult, secondResult, multiResult);
+
+		add(multiResult, firstResult, multiResult);
 	}
 }
 
 int main() {
+	setlocale(LC_ALL, "rus");
 	std::string A;
 	std::string B;
 	std::string addResult;
 	std::string multiResult;
+	std::cout << "Введите первое число...\n>>> ";
 	std::cin >> A;
+	std::cout << "\nВведите второе число...\n>>> ";
 	std::cin >> B;
-	/*add(A, B, addResult);
-	std::cout << addResult << std::endl;*/
+
+	add(A, B, addResult);
+	std::cout << "\nРезультат сложения: ";
+	std::cout << addResult << "\n\n";
+	
 	multiply(A, B, multiResult);
-	std::cout << multiResult;
+	std::cout << "Результат умножения: ";
+	std::cout << multiResult << std::endl;
+	
 	return 0;
 }
