118e0f65e8e284d120afb1e2f4bcad791143ac8a
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2022-11-04 01:33:33 +0200

    Update 8_lab.cpp

--- 8_lab/8_lab.cpp
+++ 8_lab/8_lab.cpp
@@ -3,7 +3,6 @@
 int main() {
 	long long number;
 	std::cin >> number;
-	//std::cout << (unsigned char*)(&number);
 	for (int i = 0; i < 8; i++) {
 		std::cout << (int)*((unsigned char*)(&number) + i) << " ";
 	}
