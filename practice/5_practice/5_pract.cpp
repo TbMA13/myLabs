438c67409c3efb15b2f2f56abdf537f477f4218c
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2022-11-08 09:19:03 +0200

    Работает

--- practice/5_practice/5_pract.cpp
+++ practice/5_practice/5_pract.cpp
@@ -25,8 +25,8 @@ public:
 		}
 		tempMas[m_count] = element;
 		
-		//delete[] m_mas;
-		int* m_mas = tempMas;
+		delete[] m_mas;
+		m_mas = tempMas;
 
 		m_count++;
 		std::cout << "Теперь массив имеет в конце элемент " << element << ", а его длина равна " << m_count << std::endl;
@@ -36,11 +36,15 @@ public:
 		return m_count;
 	}
 	int getElement(int index){
+		if (index >= m_count) {
+			std::cout << "Введён некорректный индекс" << std::endl;
+			return false;
+		}
 		return m_mas[index];
 	}
 	void getAllElements() {
-		for (int i = 0; i << m_count; i++) {
-			std::cout << m_mas[i] << ", ";
+		for (int i = 0; i < m_count; i++) {
+			std::cout << m_mas[i] << " ";
 		}
 		std::cout << std::endl;
 	}
@@ -51,30 +55,38 @@ public:
 	}
 
 	void delElement(int index) {
-		
 		int* tempMas = new int[m_count - 1];
+		short j = 0;
 
-		for (int i = 0; i < index; i++) tempMas[i] = m_mas[i];
-		for (int i = index + 1; i < m_count; i++) tempMas[index] = m_mas[index + 1];
-		
+		for (int i = 0; i < index; i++, j++) {
+			tempMas[j] = m_mas[i];
+		}
+		for (int i = index + 1; i < m_count; i++, j++) {
+			tempMas[j] = m_mas[i];
+		}
 		delete[] m_mas;
-		int* m_mas = tempMas;
+		m_mas = tempMas;
 
 		m_count--;
 
-		std::cout << "Элемент с индексом " << index << " удалён";
+		std::cout << "Элемент с индексом " << index << " удалён" << std::endl;
 	}
 
 	void insertElement(int element, int index) {
-
 		int* tempMas = new int[m_count + 1];
+		short j = 0;
 
-		for (int i = 0; i < index; i++) tempMas[i] = m_mas[i];
+		for (int i = 0; i < index; i++, j++) {
+			tempMas[j] = m_mas[i];
+		}
 		tempMas[index] = element;
-		for (int i = index + 1; m_count + 1; i++) tempMas[i] = m_mas[i - 1];
+		j++;
+		for (int i = index + 1; i < m_count + 1; i++, j++) {
+			tempMas[j] = m_mas[i - 1];
+		}
 
 		delete[] m_mas;
-		int* m_mas = tempMas;
+		m_mas = tempMas;
 
 		m_count++;
 
@@ -84,8 +96,36 @@ public:
 
 int main() {
 	setlocale(LC_ALL, "RUS");
+
 	dynamicMas test(1);
+	std::cout << std::endl;
+
 	test.newElementInTheEnd(2);
+	test.getAllElements();
+	std::cout << std::endl;
+
 	test.newElementInTheEnd(3);
 	test.getAllElements();
+	std::cout << std::endl;
+
+	test.setElement(12, 0);
+	test.getAllElements();
+	std::cout << std::endl;
+
+	test.delElement(0);
+	test.getAllElements();
+	std::cout << std::endl;
+
+	test.insertElement(7, 1);
+	test.getAllElements();
+	std::cout << std::endl;
+
+	std::cout << "Всего элементов:" << test.getCount() << std::endl;
+	std::cout << std::endl;
+
+	std::cout << test.getElement(0) << std::endl;
+	std::cout << test.getElement(1) << std::endl;
+	std::cout << test.getElement(2) << std::endl;
+	std::cout << test.getElement(3) << std::endl;
+	std::cout << std::endl;
 }	
