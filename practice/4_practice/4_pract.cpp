46c192972465cb19415a7ad849b168f205b0c922
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2022-12-17 11:34:32 +0200

    Update 4_pract.cpp

--- practice/4_practice/4_pract.cpp
+++ practice/4_practice/4_pract.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
 int main() {
-	// unsigned char char2 = 52; // 0011 0100 = 3 4
+
+    // unsigned char char2 = 52; // 0011 0100 = 3 4
 	//std::cout << (int) * (&char2);
+
 	setlocale(LC_ALL, "RUS");
 	short n;
 	std::cout << "Укажите количество чисел\n>>> ";
 	std::cin >> n;
 	std::cout << std::endl;
 	std::cout << "Введите любые числа из диапазона [0, 8]\nКаждое число на новой строке\n";
-	short currentNumber = 0;
-	short currentNumber2 = 0;
 	unsigned char* numbersMas = new unsigned char[n];
 	short count = -1;
 	
@@ -28,17 +28,13 @@ int main() {
 		else {
 			currentNumber2 = 15;
 		}
-		int binTempValue = 11111111;
 		short decTempValue = (currentNumber << 4) | currentNumber2;
 		numbersMas[count % 2 == 0 ? count >> 1 : (count - 1) >> 1] = decTempValue;
 	}
 	for (short i = 0; i < (count % 2 == 0 ? count >> 1 : (count - 1) >> 1) + 1; i++) {
-		short tempNumber1 = 0;
-		short tempNumber2 = 0;
 		short currentNumber = (int)(numbersMas[i]);
 		short currentNumber2 = ((currentNumber >> 3) % 2) << 3 | ((currentNumber >> 2) % 2) << 2 | ((currentNumber >> 1) % 2) << 1 | (currentNumber % 2);
 		short currentNumber1 = ((currentNumber >> 7) % 2) << 3 | ((currentNumber >> 6) % 2) << 2 | ((currentNumber >> 5) % 2) << 1 | ((currentNumber >> 4) % 2);
-		short j = 0;
 
 		std::cout << currentNumber1 << std::endl;
 
